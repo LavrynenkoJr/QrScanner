1b494a46594a7d52795f2bbe0d25fcb888c5bc10
Cyborg <s.o.lavrynenko@gmail.com>
2017-03-30 19:28:16 +0300

    Initial commit

---
+++ app/src/main/java/com/qrscanner/MainActivity.java
@@ -0,0 +1,60 @@
+package com.qrscanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.zxing.client.android.CaptureActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),CaptureActivity.class);
+                intent.setAction("com.google.zxing.client.android.SCAN");
+                intent.putExtra("SAVE_HISTORY", false);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+
+                Glide
+                        .with(this)
+                        .load(contents)
+                        .thumbnail(0.1f)
+                        .crossFade()
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .placeholder(R.drawable.noimagefound)
+                        .crossFade()
+                        .into(imageView);
+
+
+            } else if (resultCode == RESULT_CANCELED) {
+                System.out.println("RESULT_CANCELED");
+            }
+        }
+    }
+}
